abe0d968fa0ede6dfcc40d13d2a742145dbf7086
Derek Felson <drfelson@gmail.com>
2016-04-20 16:29:19 -0400

    Now only tests once, but can be edited easily to do more.

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -28,21 +28,73 @@ morfessor="../build/morfessor"
 morfessorRef="./morfessor-reference.perl"
 evalscript="./morpho-challenge-eval.perl"
 
-wordlistEng="../testdata/morpho-challenge-2005-wordlist-english.txt"
-testlistEng="../testdata/morpho-challenge-2005-testset-english.txt"
-goldstdEng="../testdata/morpho-challenge-2005-goldstd-english.txt"
+wordlist="../testdata/morpho-challenge-2005-wordlist"
+testlist="../testdata/morpho-challenge-2005-testset"
+goldstd="../testdata/morpho-challenge-2005-goldstd"
 
-outdir="results/mc2005/baseline/english"
-mkdir -p "$outdir"
-time "$morfessor" --data "$wordlistEng"
-#"$morfessor" --data "$wordlistEng" > "$outdir/model.txt"
-#"$morfessor" --load "$outdir/model.txt" --data "$testlistEng" > "$outdir/test-segmentation.txt"
-#"$evalscript" -trace -desired "$goldstdEng" -suggested "$outdir/test-segmentation.txt" > "$outdir/results.txt"
+evaluate() {
+    outdir="$1/$3"
+    args="$2"
+    language="$3"
 
-outdir="results/mc2005/baseline/reference/english"
-mkdir -p "$outdir"
-time "$morfessorRef" -data "$wordlistEng"
-#"$morfessorRef" -data "$wordlistEng" > "$outdir/model.txt"
-#"$morfessorRef" -load "$outdir/model.txt" -data "$testlistEng" | sed -e "s/^1 //" -e "s/ + / /g" -e "/^#.*/d" > "$outdir/test-segmentation.txt"
-#"$evalscript" -trace -desired "$goldstdEng" -suggested "$outdir/test-segmentation.txt" > "$outdir/results.txt"
+    mkdir -p "$outdir"
+    "$morfessor" $args --data "$wordlist-${language}.txt" > "$outdir/model.txt"
+    "$morfessor" $args --load "$outdir/model.txt" --data "$testlist-${language}.txt" > "$outdir/test-segmentation.txt"
+    "$evalscript" -trace -desired "$goldstd-${language}.txt" -suggested "$outdir/test-segmentation.txt" > "$outdir/results.txt"
+    return 0
+}
 
+evaluate_ref() {
+    outdir="$1/$3"
+    args="$2"
+    language="$3"
+
+    mkdir -p "$outdir"
+    "$morfessorRef" -data "$wordlist-${language}.txt" > "$outdir/model.txt"
+    "$morfessorRef" -load "$outdir/model.txt" -data "$testlist-${language}.txt" | sed -e "s/^1 //" -e "s/ + / /g" -e "/^#.*/d" > "$outdir/test-segmentation.txt"
+    "$evalscript" -trace -desired "$goldstd-${language}.txt" -suggested "$outdir/test-segmentation.txt" > "$outdir/results.txt"
+    return 0
+}
+
+set -o xtrace
+
+evaluate "results/mc2005/baseline" "--mode Baseline" "english"
+
+# Can run many different tests to see what effect different algorithm
+# versions or parameters may have. Uncomment as needed, or write your
+# own custom invocations.
+
+#evaluate "results/mc2005/baseline" "--mode Baseline" "english"
+#evaluate "results/mc2005/baseline-length" "--mode Length --beta 1.0 --most_common_length 7.0" "english"
+#evaluate "results/mc2005/baseline-frequency" "--mode Freq --hapax 0.5" "english"
+#evaluate "results/mc2005/baseline-frequency-length" "--mode FreqLength --beta 1.0 --most_common_length 7.0 --hapax 0.5" "english"
+#evaluate "results/mc2005/baseline-length-1.0-6.0" "--mode Length --beta 1.0 --most_common_length 6.0" "english"
+#evaluate "results/mc2005/baseline-length-1.0-7.0" "--mode Length --beta 1.0 --most_common_length 7.0" "english"
+#evaluate "results/mc2005/baseline-length-1.0-8.0" "--mode Length --beta 1.0 --most_common_length 8.0" "english"
+#evaluate "results/mc2005/baseline-length-1.0-9.0" "--mode Length --beta 1.0 --most_common_length 9.0" "english"
+#evaluate "results/mc2005/baseline-length-1.0-10.0" "--mode Length --beta 1.0 --most_common_length 10.0" "english"
+#evaluate "results/mc2005/baseline-length-1.0-11.0" "--mode Length --beta 1.0 --most_common_length 11.0" "english"
+#evaluate "results/mc2005/baseline-length-1.0-12.0" "--mode Length --beta 1.0 --most_common_length 12.0" "english"
+#evaluate "results/mc2005/baseline-length-1.0-13.0" "--mode Length --beta 1.0 --most_common_length 13.0" "english"
+#evaluate "results/mc2005/baseline-length-1.0-14.0" "--mode Length --beta 1.0 --most_common_length 14.0" "english"
+#evaluate "results/mc2005/baseline-length-1.0-15.0" "--mode Length --beta 1.0 --most_common_length 15.0" "english"
+#evaluate "results/mc2005/baseline-length-1.0-16.0" "--mode Length --beta 1.0 --most_common_length 16.0" "english"
+#evaluate "results/mc2005/baseline-length-1.0-17.0" "--mode Length --beta 1.0 --most_common_length 17.0" "english"
+#evaluate "results/mc2005/baseline-frequency-0.1" "--mode Freq --hapax 0.1" "english"
+#evaluate "results/mc2005/baseline-frequency-0.2" "--mode Freq --hapax 0.2" "english"
+#evaluate "results/mc2005/baseline-frequency-0.3" "--mode Freq --hapax 0.3" "english"
+#evaluate "results/mc2005/baseline-frequency-0.4" "--mode Freq --hapax 0.4" "english"
+#evaluate "results/mc2005/baseline-frequency-0.5" "--mode Freq --hapax 0.5" "english"
+#evaluate "results/mc2005/baseline-frequency-0.6" "--mode Freq --hapax 0.6" "english"
+#evaluate "results/mc2005/baseline-frequency-0.7" "--mode Freq --hapax 0.7" "english"
+#evaluate "results/mc2005/baseline-frequency-0.8" "--mode Freq --hapax 0.8" "english"
+#evaluate "results/mc2005/baseline-frequency-0.9" "--mode Freq --hapax 0.9" "english"
+#evaluate "results/mc2005/baseline-frequency-length-1.0-6.0-0.4" "--mode FreqLength --beta 1.0 --most_common_length 6.0 --hapax 0.4" "english"
+#evaluate "results/mc2005/baseline" "--mode Baseline" "finnish"
+#evaluate "results/mc2005/baseline-length" "--mode Length --beta 1.0 --most_common_length 7.0" "finnish"
+#evaluate "results/mc2005/baseline-frequency" "--mode Freq --hapax 0.5" "finnish"
+#evaluate "results/mc2005/baseline-frequency-length" "--mode FreqLength --beta 1.0 --most_common_length 7.0 --hapax 0.5" "finnish"
+#evaluate_ref "results/mc2005/baseline/reference" "-trace 0" "english"
+#evaluate_ref "results/mc2005/baseline/reference" "-trace 31" "finnish"
+
+set +o xtrace
